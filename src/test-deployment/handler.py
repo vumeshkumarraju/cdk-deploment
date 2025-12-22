09f686190c90695b665a14baaec654feba722c54
Umesh <umesh.raju021@gmail.com>
2025-12-22 18:39:45 +0530

    added llint and test

--- src/test-deployment/handler.py
+++ src/test-deployment/handler.py
@@ -1,9 +1,11 @@
-import json 
+import json
+
 
 def handler(event, context):
+    """AWS Lambda handler."""
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message":"This is a test route for deployment automation"
+            "message": "This is a test route for deployment automation"
         })
     }
