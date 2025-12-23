d9230cb57416e3c63a147156c416935ec8537646
Umesh <umesh.raju021@gmail.com>
2025-12-23 10:17:39 +0530

    added iam auth

--- iac/lib/iac-stack.ts
+++ iac/lib/iac-stack.ts
@@ -3,7 +3,7 @@ import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import * as apigwv2 from 'aws-cdk-lib/aws-apigatewayv2';
 import * as integrations from 'aws-cdk-lib/aws-apigatewayv2-integrations';
-
+import * as authorizers from 'aws-cdk-lib/aws-apigatewayv2-authorizers';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class IacStack extends cdk.Stack {
@@ -16,7 +16,19 @@ export class IacStack extends cdk.Stack {
       code: lambda.Code.fromAsset('../src/test-deployment'),
     });
 
-    const api = new apigwv2.HttpApi(this, 'TestApi', { apiName: 'test-api' });
+    const api = new apigwv2.HttpApi(this, 'TestApi', {
+      apiName: 'test-api',
+      defaultAuthorizer: new authorizers.HttpIamAuthorizer(),
+    });
+    // Rate Limiting (Stage Throttling)
+    const stage = api.defaultStage?.node.defaultChild as apigwv2.CfnStage;
+    if (stage) {
+      stage.defaultRouteSettings = {
+        throttlingBurstLimit: 100,
+        throttlingRateLimit: 50,
+      };
+    }
+
     api.addRoutes({
       path: '/test',
       methods: [apigwv2.HttpMethod.GET],
