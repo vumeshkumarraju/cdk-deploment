b520f412390f6ed9abdc2a1136d0949c0edab6f6
Umesh <umesh.raju021@gmail.com>
2025-12-23 12:17:55 +0530

    fixed

--- src/test-deployment/handler.py
+++ src/test-deployment/handler.py
@@ -2,8 +2,9 @@
 import json
 
 
-def handler():
+def handler(event, context):
     """AWS Lambda handler."""
+    # pylint: disable=unused-argument
     return {
         "statusCode": 200,
         "body": json.dumps({
